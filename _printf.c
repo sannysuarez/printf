9bc582fd564a5ce0bdd0c2626cb21e6fd6cac724
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-24 20:06:30 +0100

    fix it

--- _printf.c
+++ _printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
  * @format: format.
@@ -9,10 +7,8 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int i, printed_chars = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -23,44 +19,28 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			printed_chars += _putchar(format[i]);
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			i++; // Move past the '%'
+			switch (format[i])
+			{
+				case 'c':
+					printed_chars += _putchar(va_arg(list, int));
+					break;
+				case 's':
+					printed_chars += print_string(va_arg(list, char *)); // Fixed the function call
+					break;
+				default:
+					printed_chars += _putchar('%');
+					printed_chars += _putchar(format[i]);
+					break;
+			}
 		}
 	}
-
-	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
 	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
 }
