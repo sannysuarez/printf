106aea5e873143ad3c1f60eb791b616a235698fa
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-25 20:47:42 +0100

    hott fixx

--- _printf.c
+++ _printf.c
@@ -1,46 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed_chars = 0;
-	va_list list;
+	unsigned int i = 0, lent = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	if (format == NULL)
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			printed_chars += _putchar(format[i]);
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), lent++, i--;
+				}
+				else
+				{
+					lent += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
 		else
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					printed_chars += _putchar(va_arg(list, int));
-					break;
-				case 's':
-					printed_chars += print_string(va_arg(list, char *));
-					break;
-				default:
-					printed_chars += _putchar('%');
-					printed_chars += _putchar(format[i]);
-					break;
-			}
-		}
+			handl_buf(buffer, format[i], ibuf), lent++;
+		for (ibuf = lent; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-
-	va_end(list);
-
-	return (printed_chars);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (lent);
 }
