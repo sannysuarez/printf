3ebaf78ed53b85dd8a9dc8fc8bf18b4081616431
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-25 21:09:26 +0100

    func header

--- main.h
+++ main.h
@@ -20,6 +20,10 @@ typedef struct print
 } print_t;
 
 int _printf(const char *format, ...);
+void handl_buf(char *buffer, char c, unsigned int ibuf);
+unsigned int ev_print_func(const char *format, unsigned int i);
+void print_buf(char *buffer, unsigned int ibuf);
+
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
 int print_str(va_list arguments, char *buf, unsigned int ibuf);
@@ -49,13 +53,6 @@ int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinsint(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
-int ev_print_func(const char *s, int index);
-unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-char *fill_oct_array(char *bnr, char *oct);
-char *fill_long_oct_array(char *bnr, char *oct);
-char *fill_short_oct_array(char *bnr, char *oct);
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+
 
 #endif
