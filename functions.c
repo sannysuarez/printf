86e5dee89fc3e6834dacb4bc55d14cf09245f494
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-26 20:41:29 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -37,10 +37,6 @@ int print_string(va_list types, char buffer[],
 	int length = 0, i;
 	char *str = va_arg(types, char *);
 
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
 	if (str == NULL)
 	{
 		str = "(null)";
