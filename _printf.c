c6c92297e74b4d409d77ada6a3304830c3fc0b97
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-26 23:39:59 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #define BUFF_SIZE 1024
 
-
+void print_buffer(char buffer[], int *buff_ind);
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
