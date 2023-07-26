db5b0ebcd21ff951842cd1eea221d64d1b2f12c3
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-26 21:07:39 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -101,7 +101,7 @@ int print_percent(va_list types, char buffer[],
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int i, int size)
 {
-	int i = BUFF_SIZE - 2;
+        i = BUFF_SIZE - 2;
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
 	unsigned long int num;
