1fa4982eaecc28f5b4328aec18d37cffda36620b
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-26 23:25:24 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -139,8 +139,6 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
-	UNUSED(width);
-
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
