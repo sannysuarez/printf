3aeeced98fc362fb707abb446d373d9f3fac7f68
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2023-07-27 21:15:24 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -31,8 +31,7 @@ int my_char(va_list types)
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int my_string(va_list type, int flags, int width, int precision)
 {
 	int length = 0, i;
 	char *str = va_arg(types, char *);
